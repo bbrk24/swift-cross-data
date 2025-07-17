cbec1611e5759caa3bbeb624b1a7e0afd73739b9
William Baker <bbrk24@gmail.com>
2025-07-16 20:06:11 -0400

    Prevent invalid column names

--- Sources/SwiftCrossDataMacros/ModelMacro.swift
+++ Sources/SwiftCrossDataMacros/ModelMacro.swift
@@ -4,6 +4,84 @@ import SwiftDiagnostics
 import MacroToolkit
 
 public enum ModelMacro: MemberMacro, PeerMacro, ExtensionMacro {
+    // Properties and zero-argument methods of NSManagedObject (and its superclass, NSObject)
+    // cannot be column names due to CoreData limitations. Because extensions can add whatever it's
+    // impossible for a macro to detect them all, but here's a list of things that may conflict.
+    static let objcInvalidColumnNames: Set<String> = [
+        // NSObjectProtocol
+        "hash", "self", "isProxy", "retain", "release", "autorelease", "retainCount", "zone",
+        "class", "superclass", "description", "debugDescription",
+        // NSObject
+        "init", "copy", "mutableCopy", "dealloc", "autoContentAccessingProxy",
+        "classForArchiver", "classForCoder", "classForKeyedArchiver", "attributeKeys",
+        "classDescription", "toManyRelationshipKeys", "toOneRelationshipKeys", "classCode",
+        "className", "browserAccessibilitySelectedTextRange", "browserAccessibilityContainerType",
+        "browserAccessibilityCurrentStatus", "browserAccessibilityHasDOMFocus",
+        "browserAccessibilityIsRequired", "browserAccessibilityPressedState",
+        "browserAccessibilityRoleDescription", "browserAccessibilitySortDirection",
+        "accessibilityNotifiesWhenDestroyed", "objectSpecifier", "actionProperty", "awakeFromNib",
+        "imageRepresentation", "imageRepresentationType", "imageSubtitle", "imageTitle", "imageUID",
+        "imageVersion",
+        // UIAccessibilityAction
+        "accessibilityActivate", "accessibilityIncrement", "accessibilityDecrement",
+        "accessibilityPerformEscape", "accessibilityPerformMagicTap", "accessibilityCustomActions",
+        // UIAccessibilityFocus
+        "accessibilityElementDidBecomeFocused", "accessibilityElementDidLoseFocus",
+        "accessibilityElementIsFocused", "accessibilityAssistiveTechnologyFocusedIdentifiers",
+        // UIAccessibilityDragging
+        "accessibilityDragSourceDescriptors", "accessibilityDropPointDescriptors",
+        // NSKeyValueObserving
+        "observationInfo",
+        // NSKeyValueBindingCreation
+        "exposedBindings",
+        // WebPlugInContainer
+        "webFrame", "webPlugInContainerSelectionColor",
+        // WebPlugIn
+        "objectForWebScript", "webPlugInDestroy", "webPlugInInitialize", "webPlugInStart",
+        "webPlugInStop", "webPlugInMainResourceDidFinishLoading",
+        // WebScripting
+        "finalizeForWebScript",
+        // NSManagedObject
+        "entity", "objectID", "managedObjectContext", "hasChanges", "isInserted", "isUpdated",
+        "isDeleted", "isFault", "faultingState", "hasPersistentChangedValues", "awakeFromFetch",
+        "awakeFromInsert", "changedValues", "changedValuesForCurrentEvent", "prepareForDeletion",
+        "willSave", "didSave", "willTurnIntoFault", "didTurnIntoFault", "validateForDelete",
+        "validateForInsert", "validateForUpdate", "observationInfo",
+        // big undocumented blob at the bottom of the NSObject page
+        "accessibilityActivateBlock", "accessibilityActivationPoint",
+        "accessibilityActivationPointBlock", "accessibilityAttributedHint",
+        "accessibilityAttributedHintBlock", "accessibilityAttributedLabel",
+        "accessibilityAttributedLabelBlock", "accessibilityAttributedUserInputLabels",
+        "accessibilityAttributedUserInputLabelsBlock", "accessibilityAttributedValue",
+        "accessibilityAttributedValueBlock", "accessibilityContainerType",
+        "accessibilityContainerTypeBlock", "accessibilityCustomActionsBlock",
+        "accessibilityCustomRotors", "accessibilityCustomRotorsBlock",
+        "accessibilityDecrementBlock", "accessibilityDirectTouchOptions", "accessibilityElements",
+        "accessibilityElementsBlock", "accessibilityElementsHidden",
+        "accessibilityElementsHiddenBlock", "accessibilityExpandedStatus",
+        "accessibilityExpandedStatusBlock", "accessibilityFocusedUIElement", "accessibilityFrame",
+        "accessibilityFrameBlock", "accessibilityHeaderElements",
+        "accessibilityHeaderElementsBlock", "accessibilityHint", "accessibilityHintBlock",
+        "accessibilityIdentifierBlock", "accessibilityIncrementBlock", "accessibilityLabel",
+        "accessibilityLabelBlock", "accessibilityLanguage", "accessibilityLanguageBlock",
+        "accessibilityMagicTapBlock", "accessibilityNavigationStyle",
+        "accessibilityNavigationStyleBlock", "accessibilityNextTextNavigationElement",
+        "accessibilityNextTextNavigationElementBlock", "accessibilityPath",
+        "accessibilityPathBlock", "accessibilityPerformEscapeBlock",
+        "accessibilityPreviousTextNavigationElement",
+        "accessibilityPreviousTextNavigationElementBlock", "accessibilityRespondsToUserInteraction",
+        "accessibilityRespondsToUserInteractionBlock",
+        "accessibilityShouldGroupAccessibilityChildrenBlock", "accessibilityTextInputResponder",
+        "accessibilityTextInputResponderBlock", "accessibilityTextualContext",
+        "accessibilityTextualContextBlock", "accessibilityTraits", "accessibilityTraitsBlock",
+        "accessibilityUserInputLabels", "accessibilityUserInputLabelsBlock", "accessibilityValue",
+        "accessibilityValueBlock", "accessibilityViewIsModal", "accessibilityViewIsModalBlock",
+        "automationElements", "automationElementsBlock", "isAccessibilityElement",
+        "isAccessibilityElementBlock", "isSelectable", "shouldGroupAccessibilityChildren",
+        "accessibilityElementCount", "accessibilityLineEndPositionFromCurrentSelection",
+        "accessibilityLineStartPositionFromCurrentSelection",
+    ]
+
     // MARK: Extension Macro
     public static func expansion(
         of node: AttributeSyntax,
@@ -31,6 +109,23 @@ public enum ModelMacro: MemberMacro, PeerMacro, ExtensionMacro {
             }
 
             let columnName = property.identifier
+
+            if ModelMacro.objcInvalidColumnNames.contains(columnName) {
+                context.diagnose(
+                    Diagnostic(
+                        node: property._syntax,
+                        message: Message(
+                            message:
+                                "Column name cannot be a property or zero-argument method of NSManagedObject",
+                            diagnosticID: MessageID(
+                                domain: errorDomain,
+                                id: "NSManagedObjectSelectorConflict"
+                            ),
+                            severity: .error
+                        )
+                    )
+                )
+            }
 
             sqliteInitStatements +=
                 "self.\(property.identifier) = try SwiftCrossData.decodeRowValue(row, \"\(columnName)\")\n"
